49c1d8503d07a4bec0b9e2a59b5a87d2dfa7f94b
Tenji <patricepetit@hotmail.com>
2021-04-15 20:07:09 +0200

    feature/dist: add addImage, close app on fullscreen

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const electron = require('electron')
+console.log(electron)
+const ipcRenderer = electron.ipcRenderer;
+
+ipcRenderer.on("cpu", (event, data) => {
+
+  document.getElementById("cpu").innerHTML = data.toFixed(2)
+})
+ipcRenderer.on("mem", (event, data) => {
+  document.getElementById("mem").innerHTML = data.toFixed(2)
+
+})
+ipcRenderer.on("total-mem", (event, data) => {
+  document.getElementById("total-mem").innerHTML = data.toFixed(2)
+})
+
+window.onload = function ready() {
+  const close = document.getElementById("close-button")
+	if (close) {
+		close.addEventListener('click', () => {
+			console.log('close')
+
+			if (electron.remote) {
+				electron.remote.getCurrentWindow().close()
+			} else {
+				ipcRenderer.send('close')
+			}
+		})
+	}
+
+}
